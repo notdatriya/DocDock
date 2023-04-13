8fb3b7892692e6fb5824e7dd3a37b3c411d1abf9
Riya <harryy.6066@gmail.com>
2023-04-13 12:21:37 +0530

    db changes

--- db.js
+++ db.js
@@ -1,20 +1,15 @@
-const mysql=require('mysql');
+
+require('dotenv').config()
+const mysql= require('mysql');
 
 let dbconnection=mysql.createPool({
     host:'sql12.freemysqlhosting.net',
-    user:'sql12611409',
-    password:'cSbZ6gmQuL',
-    database:'sql12611409'
+    user:process.env.user,
+    password:process.env.password,
+    database:'sql12611473'
 });
 
-// connection.connect((err)=>{
-//     if(err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log('db connected');
-//     }
-// });
+
 
 module.exports=dbconnection;
 
